02e7d3c35e88765ac10e9eff52a7936ec44c2a3e
PaulOkoh <pauldokoh@gmail.com>
2022-10-17 20:50:00 -0500

    add png images and icons to assets folder

---
+++ script.js
@@ -0,0 +1,16 @@
+// let arr = ["I", "wish", "I", "hadn't", "come"]
+
+// function partlist(arr) {
+//   var newArray;
+//   var returnArray=[];
+//   for( var i=1; i<arr.length; i++){
+//     newArray = [];
+//     newArray.push(arr.slice(0,i).join(" "));
+//     newArray.push(arr.slice(i).join(" "));
+//     returnArray.push(newArray);
+//   }
+  
+//   return returnArray;
+// }
+
+// partlist(arr);
